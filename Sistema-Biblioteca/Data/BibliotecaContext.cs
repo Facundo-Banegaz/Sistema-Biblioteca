5141208d2febe16d245a3030ff8bd629f12776f2
Facundo-Banegaz <Banegaz@users.noreply.github.com>
2024-07-31 00:34:47 -0300

    Se crearon solo las vistas del sistema

---
+++ Sistema-Biblioteca/Data/BibliotecaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sistema_Biblioteca.Data
+{
+    public class BibliotecaContext : IdentityDbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options)
+            : base(options)
+        {
+        }
+    }
+}
